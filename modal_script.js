dc181fc5559ebca30595ccf8d06afb1abdad0251
Zinedine Chami <zinedine.chami@gmail.com>
2024-01-01 12:59:01 +0100

    created modal opening and closing functions, started styling in css

--- modal_script.js
+++ modal_script.js
@@ -1,5 +1,24 @@
+const modal_button = document.querySelector(".js-modal");
+
+console.log(modal_button)
+
 const openModal = function (event) {
+    event.preventDefault();
+    const target = document.querySelector(event.target.getAttribute('href'))
+    target.style.display = null
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.modal_close').addEventListener('click', closeModal)
 
 }
 
-document.querySelector('modal').addEventListener('click')
+const closeModal = function(event) {
+    if (modal === null) return
+    event.preventDefault();
+    modal.style.display = "none"
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.modal_close').removeEventListener('click', closeModal)
+    modal = null
+}
+
+modal_button.addEventListener('click', openModal)
